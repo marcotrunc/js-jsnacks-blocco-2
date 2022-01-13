28e0f9dde5ce10b673969b0e86865728878e3579
marcotrunc <marcotrunc@gmail.com>
2022-01-13 16:40:52 +0100

    added exercise 2 with bonus

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ** Esercizio 1 
+// ! Esercizio 1 
 /*Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.
@@ -43,5 +43,49 @@ numberButton.addEventListener('click', function(){
         labelNumber.classList.add('d-none');
     }
 }); 
-    
-      
+
+// ! Esercizio 2
+/*Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo
+Javascript - JS
+JSnack 2 */
+
+// Recupero Elementi in pagina
+const labelNumber2 = document.getElementById('label-number-2');
+const numberField2 = document.getElementById('number-2');
+const numberButton2 =document.getElementById('number-button-2');
+const positiveField = document.getElementById('positive');
+const allNumber =document.getElementById('all-number');
+// Dichiaro l'array e l'index
+const positiveArray = [];
+let j = 0;
+// Al click acquisisco i valori e li metto nell'array
+numberButton2.addEventListener('click', function(){
+    numberValue2 = numberField2.value;
+    console.log(numberValue2);
+    positiveArray.push(numberValue2);
+    // Ipotizzo che il mio array contenga massimo 5 numeri
+    if(positiveArray.length === 5){
+        let allNumberElement = '';
+        let listPositiveNumber = ' ';
+        // Compongo il ciclo while
+        while(j < 5){
+        allNumberElement +=  positiveArray[j];
+        allNumber.innerText = allNumberElement;
+        // Dichiarazione Elemento dell'array
+        let positiveArrayElement = positiveArray[j];
+        if(positiveArrayElement % 2 === 0){
+            // Stampo elemento pari
+            listPositiveNumber += positiveArrayElement;
+            positiveField.innerHTML =listPositiveNumber + ' ';
+            console.log(positiveArrayElement);
+        }
+        // Condizione d'uscita
+        j++
+        }
+        // Tolgo l'input
+        numberField2.classList.add('d-none');
+        numberButton2.classList.add('d-none');
+        labelNumber2.classList.add('d-none');
+    }
+});
